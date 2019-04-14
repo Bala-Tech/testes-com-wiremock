4b96b4f50bb756424499aa3ad5466db2ad7bd69f
Bala-Tech <balatecnologia@gmail.com>
2019-04-14 12:03:44 -0300

    wiremock modo standalone

---
+++ tutoriais/3-executando-wiremock-modo-standalone/address-register-api/src/main/java/br/com/balatech/addressregister/AddressRegisterApplication.java
@@ -0,0 +1,12 @@
+package br.com.balatech.addressregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AddressRegisterApplication.class, args);
+    }
+}
---
+++ tutoriais/3-executando-wiremock-modo-standalone/address-register-api/src/main/java/br/com/balatech/addressregister/bean/ZipCodeResponse.java
@@ -0,0 +1,59 @@
+package br.com.balatech.addressregister.bean;
+
+public class ZipCodeResponse {
+
+    private String zipCode;
+    private String address;
+    private String complement;
+    private String neighborhood;
+    private String state;
+    private String city;
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getComplement() {
+        return complement;
+    }
+
+    public void setComplement(String complement) {
+        this.complement = complement;
+    }
+
+    public String getNeighborhood() {
+        return neighborhood;
+    }
+
+    public void setNeighborhood(String neighborhood) {
+        this.neighborhood = neighborhood;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
